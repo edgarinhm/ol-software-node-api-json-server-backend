45b55a192233e44b51521f061a41f91206b05c33
Edgar Mejia <edgarinhm@hotmail.com>
2024-06-17 23:51:40 -0500

    change port validation

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middleware = jsonServer.defaults();
-const port = proccess.env.PORT ?? 8080
+const port = proccess.env.PORT || 8080;
 
 server.use(middleware);
 server.use(router);
